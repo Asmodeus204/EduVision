edc4dad8edb8093cdb95b59bbae7d07b9da66c09
Hiếu Đạt <Hieudat2310.bh@gmail.com>
2023-11-03 23:02:31 +0700

    23h (11/3)

---
+++ ExamWebApp/ExamWebApp/ExamWebApp/Models/CountryModels.cs
@@ -0,0 +1,17 @@
+﻿using ExamWebApp.Data;
+using ExamWebApp.Utils;
+using Microsoft.Extensions.Logging;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamWebApp.Models
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+
+}
