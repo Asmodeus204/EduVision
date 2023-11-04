56a9a1c5ac14bba239f86b022af8e55d64e6e55d
Hiếu Đạt <Hieudat2310.bh@gmail.com>
2023-11-04 11:50:49 +0700

    11"50 (04/11)

---
+++ ExamWebApp/ExamWebApp/ExamWebApp/Models/ErrorLogModels.cs
@@ -0,0 +1,20 @@
+﻿using ExamWebApp.Data;
+using ExamWebApp.Utils;
+using Microsoft.Extensions.Logging;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamWebApp.Models
+{
+    public class ErrorLog
+    {
+        [Key]
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string ErrorMessage { get; set; }
+        public string ErrorDetails { get; set; }
+        public DateTime? ErrorDate { get; set; }
+    }
+
+
+}
---
+++ ExamWebApp/ExamWebApp/ExamWebApp/Utils/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ExamWebApp.Models;
+using System.IO.Packaging;
+
+namespace ExamWebApp.Utils
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<StudentAnswer, StudentAnswerCloned>();
+        }
+    }
+
+}
